ed33199879433fea6b4c5d523f2af8b3206e69fd
https://github.com/nbcf/ProjetoDeBolso/commits?author=nbcf <>
2022-12-10 14:34:40 -0300

    teste conta

--- src/InfraTi/lerDadosDebug.java
+++ src/InfraTi/lerDadosDebug.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package InfraTi;
 
 import java.io.DataInputStream;
@@ -12,7 +8,7 @@ import java.io.ObjectInputStream;
 
 /**
  *
- * @author Nildo
+ * @author Nildo  
  */
 public class lerDadosDebug {
  
